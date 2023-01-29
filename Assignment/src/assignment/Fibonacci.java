3c5cf38074eff5a3d0df67504a5f24694397b53d
Ratan <ratanjn@bu.edu>
2023-01-28 19:09:16 -0500

    Fibonacci changes

--- Assignment/src/assignment/Fibonacci.java
+++ Assignment/src/assignment/Fibonacci.java
@@ -1,5 +1,8 @@
 package assignment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fibonacci {
 	public static long fibNumber(long number) {
 		if(number<=1) {
@@ -10,10 +13,11 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Long> seq=new ArrayList<>();
 		for(int i=0;i<101;i++) {
-			System.out.println("The fibonnaci value for "+i+" is "+Fibonacci.fibNumber(i));
+			seq.add(Fibonacci.fibNumber(i));
 		}
-
+		System.out.println(seq);
 	}
 
 }
